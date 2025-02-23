ba4c77df6880886baa569dc04cd1d0531862da14
Daniel Ju WINDOWS <danjuacademic@gmail.com>
2025-02-23 18:14:32 -0500

    first draft of rotate method

--- Rotor.java
+++ Rotor.java
@@ -11,10 +11,26 @@ public class Rotor {
         while(!this.rotate());
             
     }
-    
-    public boolean rotate(){
-        //TODO
-               
+
+    //shifts rotor values one step
+    //update startChar to new first character
+    //return true if full cycle (original startChar = new startChar)
+    public boolean rotate(){ 
+        char[] charArray = rotorValues.toCharArray();
+        char holdFirst = charArray[0]; //hold the first character for comparison later
+        
+        for (int i = 0; i < charArray.length-1; i++) { //shift each character by 1 to the right
+            charArray[i] = charArray[i+1];
+        } 
+
+        charArray[charArray.length - 1] = holdFirst; //put first character to the end
+        rotorValues = new String(charArray); 
+
+        if (charArray[0] == startChar) { //check if rotor has completed a full cycle
+            return true;
+        } else {
+            return false;
+        }
     }
     
 
