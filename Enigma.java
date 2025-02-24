57f521aca2ce15f553321289d6f3d10d9f6febc7
Daniel Ju WINDOWS <danjuacademic@gmail.com>
2025-02-24 01:49:39 -0500

    fixed rotor rotate method to properly move clockwise. undid changes to rotate method in Enigma to view 0 as innermost rotor, 2 as outermost rotor.

--- Enigma.java
+++ Enigma.java
@@ -24,13 +24,11 @@ public class Enigma{
         for (int i = 0; i < message.length(); i++) {
             char outerChar2 = message.charAt(i); //get outer character from message
             int outerIndex = rotors[2].indexOf(outerChar2); //find corresponding position in outer rotor (2)
-            char middleChar1 = rotors[1].charAt(outerIndex); //get matching first middle char from outer index
-            int middleIndex = rotors[2].indexOf(middleChar1); //find corresponding position of outer char from middle char
+            char middleChar = rotors[1].charAt(outerIndex); //get matching first middle char from outer index
+            int middleIndex = rotors[2].indexOf(middleChar); //find corresponding position of outer char from middle char
             char ogChar = rotors[0].charAt(middleIndex); //get matching inner char from middle index 
             result += ogChar;
-
             rotate();
-
         }
         return result;
     }
@@ -40,16 +38,13 @@ public class Enigma{
     public String encrypt(String message){ 
         String result = "";
         for (int i = 0; i < message.length(); i++) {
-
             char ogChar = message.charAt(i); //get character 1 from message
             int innerIndex = rotors[0].indexOf(ogChar); // find corresponding position in inner rotor (0)
             char outerChar1 = rotors[2].charAt(innerIndex); // get matching outer char for first pass 
             int middleIndex = rotors[1].indexOf(outerChar1); // find corresponding position of outer char in middle rotor (1)
             char outerChar2 = rotors[2].charAt(middleIndex); // get matching char from outside rotor (2)
             result += outerChar2;
-
             rotate();
-            
         }
         return result;
     }
@@ -57,7 +52,7 @@ public class Enigma{
     
     private void rotate(){
         if(rotors[0].rotate()){
-            if(rotors[1].rotate()){
+            if(rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
--- Rotor.java
+++ Rotor.java
@@ -8,22 +8,23 @@ public class Rotor {
         this.rotorValues = new String(v);
         this.startChar = c;
         
-        while(!this.rotate());
-            
+        while (!this.rotate()) {
+            this.rotate();
+        }
     }
 
-    //shifts rotor values one step
-    //update startChar to new first character
-    //return true if full cycle (original startChar = new startChar)
+    //shifts rotor values one step to the right (clockwise)
+    //save the last character and put at the start
+    //check if a full rotation has been made
     public boolean rotate(){ 
         char[] charArray = rotorValues.toCharArray();
-        char holdFirst = charArray[0]; //hold the first character for comparison later
         
-        for (int i = 0; i < charArray.length-1; i++) { //shift each character by 1 to the right
-            charArray[i] = charArray[i+1];
+        char holdLast = charArray[charArray.length - 1];
+        for (int i = charArray.length - 1; i > 0; i--) { 
+            charArray[i] = charArray[i-1]; // shift each character to the right by setting each index to the value of the index before it
         } 
 
-        charArray[charArray.length - 1] = holdFirst; //put first character to the end
+        charArray[0] = holdLast; //put saved last character as first character
         rotorValues = new String(charArray); 
 
         if (charArray[0] == startChar) { //check if rotor has completed a full cycle
